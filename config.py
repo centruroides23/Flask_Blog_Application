50ab4908a149769f4c5b6575473d814ee42d31f6
Oscar Bejarano <oscar.bejarano503@gmail.com>
2024-05-27 11:17:34 -0600

    Change the DB connection to Vercel PostgreSQL.

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or os.urandom(32)
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///" + os.path.join(basedir, "instance/posts.db")
+    SQLALCHEMY_DATABASE_URI = "postgresql://default:************@ep-frosty-cherry-a5px8p3y.us-east-2.aws.neon.tech:5432/verceldb?sslmode=require"
